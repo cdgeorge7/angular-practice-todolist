83db9036481ac2b077dbf167f80a99d4eb034aa7
cdgeorge7 <cdgeorge7@gmail.com>
2019-08-27 12:24:40 -0500

    todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-practice-todolist';
+  name: string = "Chris";
+
+  constructor() {
+    this.changeName("Biscuits");
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
